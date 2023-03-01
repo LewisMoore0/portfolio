995202336d885f4f3953ede2717b6d25e5af1365
Lewis Moore <lewismooredev@gmail.com>
2023-03-01 18:04:43 +0000

    initial css grid comlumns

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import { Banner } from './Banner';
 function App() {
   return (
     <div className="App">
+         <Banner />
       <div className='container'>
-      <Banner />
-        <body>
-          <h1> This is the body </h1>
-        </body>
+        <div className='column'>
+            <h1> div1 </h1>
+        </div>
+        <div className='column'>
+            <h1> div2 </h1>
+        </div>
+        <div className='column'>
+            <h1> div3 </h1>
+        </div>
       </div>
     </div>
+
   );
 }
 
