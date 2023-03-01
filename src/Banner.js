e9e898ab827cdfca772ee48c9c608c63dea5f77f
Lewis Moore <lewismooredev@gmail.com>
2023-03-01 15:27:53 +0000

    Add initial banner component with some styling

---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+export const Banner = () => {
+    return (
+        <div className='Banner'>
+            <div class="navbar">
+                <a href="#" class="active">Home</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+    )
+}
