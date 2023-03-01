a908cd5c59b018a1d357b8e17f3d763b1e7727b1
Lewis Moore <lewismooredev@gmail.com>
2023-03-01 15:48:42 +0000

    Add more styling and refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import { Banner } from './Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Banner />
-      </header>
+      <div className='container'>
+      <Banner />
+        <body>
+          <h1> This is the body </h1>
+        </body>
+      </div>
     </div>
   );
 }
