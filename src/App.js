13c07e1fb1c18a41f5283af9e5ddedac340cfee5
Lewis Moore <lewismooredev@gmail.com>
2023-03-03 19:59:52 +0000

    play around with grid

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
          <Banner />
+
+      <div className='wrapper'>
       <div className='container'>
         <div className='column'>
             <h1> div1 </h1>
@@ -15,6 +17,18 @@ function App() {
         <div className='column'>
             <h1> div3 </h1>
         </div>
+      </div>
+      <div className='container'>
+        <div className='column'>
+            <h1> div1.2 </h1>
+        </div>
+        <div className='column'>
+            <h1> div2.2 </h1>
+        </div>
+        <div className='column'>
+            <h1> div3 </h1>
+        </div>
+      </div>
       </div>
     </div>
 
