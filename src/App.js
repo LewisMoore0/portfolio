7defa7bf00f981421d1a185cbf54a6b6475823a7
Lewis Moore <lewismooredev@gmail.com>
2023-03-04 16:29:17 +0000

    Play around with css grid

--- src/App.js
+++ src/App.js
@@ -4,31 +4,12 @@ import { Banner } from './Banner';
 function App() {
   return (
     <div className="App">
-         <Banner />
-
+      <Banner />
       <div className='wrapper'>
-      <div className='container'>
-        <div className='column'>
-            <h1> div1 </h1>
+          <img className='portrait' src='IMG_0168 Medium.jpeg'></img>
+        <div className='text-container'>
+          <h1>Text Container Div</h1>
         </div>
-        <div className='column'>
-            <h1> div2 </h1>
-        </div>
-        <div className='column'>
-            <h1> div3 </h1>
-        </div>
-      </div>
-      <div className='container'>
-        <div className='column'>
-            <h1> div1.2 </h1>
-        </div>
-        <div className='column'>
-            <h1> div2.2 </h1>
-        </div>
-        <div className='column'>
-            <h1> div3 </h1>
-        </div>
-      </div>
       </div>
     </div>
 
