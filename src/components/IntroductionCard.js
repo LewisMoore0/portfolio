f5989d90276bf8108e7bd26726ec824acc3eeea5
LewisMoore0 <106735867+LewisMoore0@users.noreply.github.com>
2024-11-18 11:46:02 +0100

    Adjust homepage layout

--- src/components/IntroductionCard.js
+++ src/components/IntroductionCard.js
@@ -3,14 +3,10 @@ import { Button, Grid, Typography, Box } from '@mui/material'
 export const IntroductionCard = () => {
 
     return ( 
-        <Box>
-            <Grid container>
-                <Box sx={{ textAlign: 'center', padding: 10, width: '100%'}}>
-                    <Typography variant='h3'>Hi I'm Lewis</Typography>
-                    <Typography sx={{ paddingBottom: 3}} variant='h4'>a full stack web developer</Typography>
-                    <Button variant='contained'>Get in touch</Button>
-                </Box>
-            </Grid>
+        <Box sx={{ textAlign: 'center', paddingTop: 15, height: '100%' }}>
+            <Typography variant='h3'>Hi I'm Lewis</Typography>
+            <Typography sx={{ paddingBottom: 5}} variant='h4'>a full stack web developer</Typography>
+            <Button variant='contained'>Get in touch</Button>
         </Box>
     )
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,12 @@
 import { useTheme, Box } from '@mui/material'
 import { IntroductionCard } from '../components/IntroductionCard'
+import { About } from '../components/About'
 
 export const HomePage = () => {
     const theme = useTheme()
 
     return ( 
-        <Box>
+        <Box sx={{ height: 500}}>
             <IntroductionCard/>
         </Box>
     )
