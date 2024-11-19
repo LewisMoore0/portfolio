eacfd08b25307e9b3bda5ff4c2c45e05a241cc37
LewisMoore0 <106735867+LewisMoore0@users.noreply.github.com>
2024-11-19 17:45:46 +0100

    typos

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,19 +5,19 @@ import '../styles/project.css'
 export const Project = () => {
     return (
         <div class='project-container'>
-            <a class='project-item' href='https://www.github.com/lewismo>ore0'>
+            <a class='project-item' href='https://www.github.com/lewismoore0'>
                 <div >
                     <h3>Project Number 1</h3>
                     <p>A summary of what this project is, what it does and the technology it uses</p>
                 </div>
             </a>
-            <a class='project-item' href='https://www.github.com/lewismo>ore0'>
+            <a class='project-item' href='https://www.github.com/lewismoore0'>
                 <div >
                     <h3>Project Number 2</h3>
                     <p>A summary of what this project is, what it does and the technology it uses</p>
                 </div>
             </a>
-            <a class='project-item' href='https://www.github.com/lewismo>ore0'>
+            <a class='project-item' href='https://www.github.com/lewismoore0'>
                 <div >
                     <h3>Project Number 3</h3>
                     <p>A summary of what this project is, what it does and the technology it uses but this one has a 
