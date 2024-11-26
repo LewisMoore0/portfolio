65522daa512fdb0543d82f72e2352757efe360d5
LewisMoore0 <106735867+LewisMoore0@users.noreply.github.com>
2024-11-26 14:24:50 +0000

    About section with progress bars

---
+++ src/components/About.js
@@ -0,0 +1,58 @@
+import { Grid2 as Grid, Typography, useTheme, List} from "@mui/material"
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
+
+
+
+export const About = () => {
+    const theme = useTheme()
+
+    const skillsList = [{ skill: 'Javascript', progress: 100} , {skill: 'React', progress: 75}, {skill: 'HTML & CSS', progress: 75}, {skill: 'Responsive Web Design', progress: 75}, { skill: 'Node', progress: 50}, { skill: 'SQL', progress: 50} ]
+
+    const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+        height: 10,
+        borderRadius: 5,
+        [`&.${linearProgressClasses.colorPrimary}`]: {
+          backgroundColor: theme.palette.grey[200],
+          ...theme.applyStyles('dark', {
+            backgroundColor: theme.palette.grey[800],
+          }),
+        },
+        [`& .${linearProgressClasses.bar}`]: {
+          borderRadius: 5,
+          backgroundColor: '#1a90ff',
+          ...theme.applyStyles('dark', {
+            backgroundColor: '#308fe8',
+          }),
+        },
+      }));
+
+    return (
+        <Grid container sx={{ height: '100%', backgroundColor: theme.palette.primary.main, color: 'white', padding: 5 }}>
+            <Grid item size={6} sx={{ padding: 3}}>
+                <Typography variant="h5" align="center" sx={{ padding: 1}}>Who am I?</Typography>
+                <Typography variant='h6' align="justify" sx={{ pt: 1}}> 
+                    Hi, I'm Lewis. I started my Software Development journey in 2018 when I attended and graudated from Makers Academy.
+                    I was fortunate enough to attend Makes on a fully funded fellowship, and since then, I've not looked back.
+                    Having worked for both small and large companies, I have found my passion in helping people solve problems.
+                    Now embarking on my freelancing journey, I help small and medium sized companies solve issues and create great products and websites.
+                </Typography>
+            </Grid>
+            <Grid item size={6} sx={{ padding: 3}}>
+                <Typography variant="h5" align="center" sx={{ padding: 1}}>My skills</Typography>
+                {skillsList.map((skill) => {
+                    return (
+                        <>
+                            <Typography align='justify' sx={{ padding: 1 }}>{skill.skill}</Typography>
+                            <BorderLinearProgress variant="determinate" value={skill.progress} />
+                        </>
+                    )
+                })}
+
+
+
+            </Grid>
+
+        </Grid>
+    )
+}
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import { useTheme, Box } from '@mui/material'
 import { IntroductionCard } from '../components/IntroductionCard'
 import { Project } from '../components/Project'
+import { About } from '../components/About'
 
 export const HomePage = () => {
     const theme = useTheme()
@@ -10,9 +11,13 @@ export const HomePage = () => {
             <Box sx={{ height: 500, backgroundImage: 'linear-gradient(white, #f5f7ff)' }}>
                 <IntroductionCard/>
             </Box>
+            <Box sx={{ height: 500 }}>
+                <About />
+            </Box>
             <Box sx={{ backgroundImage: 'linear-gradient(#f5f7ff, white)'}}>
                 <Project />
             </Box>
+
             
         </main>
 
